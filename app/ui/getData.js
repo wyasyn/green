a1306a87689a94c344e81aa1f38ecaa9c3e7355d
Yasin Walum <63132133+wyasyn@users.noreply.github.com>
2024-01-19 02:23:24 +0300

    axios

--- app/ui/getData.js
+++ app/ui/getData.js
@@ -1,14 +1,20 @@
 import axios from "axios";
 
 export async function getProjects() {
-    const body = await axios.get("/api/project");
+    const body = await axios.get(
+        "https://green-4xtzt7d15-wyasyn.vercel.app//api/project"
+    );
     return body.data;
 }
 export async function getBlogs() {
-    const body = await axios.get("/api/blog");
+    const body = await axios.get(
+        "https://green-4xtzt7d15-wyasyn.vercel.app//api/blog"
+    );
     return body.data;
 }
 export async function getUsers() {
-    const body = await axios.get("/api/user");
+    const body = await axios.get(
+        "https://green-4xtzt7d15-wyasyn.vercel.app//api/user"
+    );
     return body.data;
 }
